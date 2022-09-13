be3d40430b61e6b0e5dda2e2165ff63343825e4e
Sylvwar <maxmallet78@gmail.com>
2022-09-13 13:24:00 +0200

    Initial commit, React setup

---
+++ src/components/Card.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../styles/Card.module.css";
+
+export default function Card() {
+    return <div style={styles.bg}>Card</div>;
+}
---
+++ src/pages/Characters.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Characters() {
+    return <div>Characters</div>;
+}
